65dfac08bb831c7ae734cc3d91662ea24fd705e2
gajendra80300@gmail.com <gajendra80300@gmail.com>
2021-09-11 12:00:58 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.get('/',(req,res)=>{
     
     res.send("Hello world");
 })
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
